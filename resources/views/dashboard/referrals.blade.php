491e127623967f300a4476aba0aa980d17e50026
Diego <diegoalejandrogarcia140902@gmail.com>
2024-06-21 15:08:33 -0500

    Se modifico la view de referrals, y se agregaron unas migraciones

--- resources/views/dashboard/referrals.blade.php
+++ resources/views/dashboard/referrals.blade.php
@@ -3,11 +3,68 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Refeals</h1>
+    <h1>Referrals</h1>
 @stop
 
 @section('content')
-    <p>Refeals.</p>
+    <div class="row">
+
+        <div class="col-sm-3">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">User Referral Link</h5>
+                <p class="card-text"><a href="">TempLinks</a></p>
+              </div>
+            </div>
+          </div>
+
+
+        <div class="col-sm-3">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Referrals Amount</h5>
+              <p class="card-text">5</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Referral Monthly Incomes</h5>
+                <p class="card-text">$ 50.000</p>
+              </div>
+            </div>
+          </div>
+
+
+        
+        <div class="col-sm-3">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Referral Total Incomes</h5>
+              <p class="card-text"># 500.000</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <table class="table display" id="links-table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">User</th>
+            <th scope="col">Incomes</th>
+            <th scope="col">Referral Date</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+        </tbody>
+      </table>
 @stop
 
 @section('css')
@@ -16,4 +73,13 @@
 
 @section('js')
     
+<script>
+    $(document).ready(function() {
+
+        $('#links-table').DataTable({
+        "paging": true,
+        "searching": true,
+        });
+});
+</script>
 @stop
